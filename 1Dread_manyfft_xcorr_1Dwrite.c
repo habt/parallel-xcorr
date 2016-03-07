a73124d20359145240aa60f441986ef7d6f87c9d
Habte <habteke@gmail.com>
2016-03-07 14:00:42 +0200

    incorrect data distribution corrected

--- 1Dread_manyfft_xcorr_1Dwrite.c
+++ 1Dread_manyfft_xcorr_1Dwrite.c
@@ -12,6 +12,8 @@
 #define NY     1000
 #define RANK   2
 #define MAX_NAME 20
+#define MAX_NUM_DATASETS 10
+#define MAX_NUM_XCORR 45        // with 10 signals this value is equal to 9+8+7+6+5+4+3+2+1 
 
 int
 main (int argc, char **argv)
@@ -20,8 +22,9 @@ main (int argc, char **argv)
      * HDF5 APIs definitions
      */ 	
     hid_t       file_id; 
-    hid_t       dataset_id;               /* file and dataset identifiers */
-    hid_t       filespace, memspace;      /* file and memory dataspace identifiers */
+    hid_t       dataset_id[MAX_NUM_DATASETS];               /* file and dataset identifiers */
+    hid_t       filespace[MAX_NUM_DATASETS];
+    hid_t       memspace;      /* file and memory dataspace identifiers */
     hsize_t     dimsf[RANK];                 /* dataset dimensions */ 
     hsize_t     tot_proc_datasets[RANK];	         
     hsize_t	count[RANK];                 /* hyperslab selection parameters */
@@ -29,18 +32,20 @@ main (int argc, char **argv)
     hsize_t     stride[RANK];
     hsize_t     block[RANK];
     hid_t	plist_id;                 /* property list identifier */
-    int         i;
+    int         i,j;
     herr_t	status;
     hssize_t 	data_dim;
     size_t      size; 
     hid_t       datatype;
     int         rank;
     hsize_t     dims_out[RANK];
-    hsize_t     num_obj,obj_idx;
+    hsize_t     num_obj;
+    hsize_t     obj_idx;
     char        dset_name[MAX_NAME];
     size_t      name_size;
-    int         dataset_size[10];
-    int         dataset_idx[10];
+    int         dataset_size[MAX_NUM_DATASETS];
+    int         xcorr_size[MAX_NUM_XCORR];
+    hsize_t     dataset_idx[MAX_NUM_DATASETS];
     int         dataset_per_proc;
     double complex     *dset_data;//using malloc is better
     int         otype;
@@ -63,9 +68,12 @@ main (int argc, char **argv)
     ptrdiff_t local_no; 
     ptrdiff_t local_o_start;
 
-    /*Correlation variables*/
+    /* Other Correlation variables*/
     int num_computed_xcorr = 0;
     int num_xcorr = 0;
+    int set = 1;
+    int max_size = 0;
+    int sec_max_size = 0;
 
     /*
      * MPI variables
@@ -103,186 +111,155 @@ main (int argc, char **argv)
       otype =  H5Gget_objtype_by_idx(file_id, (size_t)obj_idx);
       if(otype == H5G_DATASET)
       {
+          dataset_idx[num_dataset_obj] = obj_idx;
           num_dataset_obj = num_dataset_obj +1;
-	  H5Gget_objname_by_idx(file_id, obj_idx, dset_name,(size_t)MAX_NAME );
-      	  printf("name starts with : %s \n",dset_name);
-          printf("obj_idx : %d \n",(int) obj_idx);
+      }
+      obj_idx++;
+    }
 
-      	  /* Open an existing dataset. */
-      	  dataset_id = H5Dopen(file_id, dset_name, H5P_DEFAULT);
-      
-          /* Acquire some dataset information*/
-      	  datatype  = H5Dget_type(dataset_id);
-          size  = H5Tget_size(datatype);
-          printf("size of stored datatype is : %d \n", (int) size);
-          printf("size of complex datatype : %lu \n", sizeof(fftw_complex));
-          filespace = H5Dget_space(dataset_id);
-          rank      = H5Sget_simple_extent_ndims(filespace);
-          status  = H5Sget_simple_extent_dims(filespace, dims_out, NULL);
-	  printf("dims_out[0] : %d , dims_out[1] : %d \n", (int) dims_out[0], (int) dims_out[1]);
-          if(dims_out[0]==1)
-		dataset_size[num_dataset_obj-1]=dims_out[1]; 
-          else 
-		dataset_size[num_dataset_obj-1]=dims_out[0];
-       
-          dataset_per_proc=(dataset_size[num_dataset_obj-1]/mpi_size)+0.5;   
-	  printf("dataset_per_proc : %d \n", (int) dataset_per_proc);
-          // Is it first memory space then filespace?
-          /*Create the local memory space*/
-          
-          /*
-          count[0]= dataset_per_proc;
-          count[1]= 1;
-          memspace = H5Screate_simple(RANK,count,NULL);
-          dset_data = (double *) malloc(dataset_per_proc*size);
-          */
-      
-          /*Create the filespace/dataset hyperslab*/
-          
-          if (iscomplex == false){ 		// if dataset is non-complex double values
-          	offset[0]=mpi_rank*dataset_per_proc;
-          	offset[1]=0;
-          	count[0] = dataset_per_proc;
-	  	count[1] = 1;
-		block[0]= 1; //each block is 1 value representing one non-complex number
-                block[1] = 1;
-	 }
-         else {                                 //if data set is a set of complex numbers where imaginary value is stored right after the real
-         	offset[0]=mpi_rank*dataset_per_proc*2;// starting offset is multiplied by two because each complex number is two values
-          	offset[1]=0;
-          	count[0] = dataset_per_proc; // represents the number of complex numbers
-	  	count[1] = 1;
-		block[0]= 2; //each block of 2 values represents one complex number
-                block[1] = 1;	
+    /* Loop to determine the biggest dataset size to determine the number of zeros to be appended to each signal before the FFT*/
+    /* an alternative is set a maximum length for correlation and extend each signal to tat length */
+    for(i=0;i<num_dataset_obj; i++) // NB this loop can be removed and the code written inside the while loop above
+    {
+        printf("loop : %d \n", i);
+	H5Gget_objname_by_idx(file_id, dataset_idx[i], dset_name,(size_t)MAX_NAME );
+        dataset_id[i] = H5Dopen(file_id, dset_name, H5P_DEFAULT);
+        printf("data set name %d is %s  \n", i,dset_name);
 
+	filespace[i] = H5Dget_space(dataset_id[i]);
+        rank      = H5Sget_simple_extent_ndims(filespace[i]);
+        status  = H5Sget_simple_extent_dims(filespace[i], dims_out, NULL);
+	printf("dims_out[0] : %d , dims_out[1] : %d \n", (int) dims_out[0], (int) dims_out[1]);
+        if(dims_out[0]==1)
+           dataset_size[i]=dims_out[1]; 
+        else 
+	   dataset_size[i]=dims_out[0];
+        
+        if(dataset_size[i]>sec_max_size)
+	{
+            if(dataset_size[i]>max_size)
+     	    {
+		max_size=dataset_size[i];
+	        sec_max_size = max_size;
+	    }
+	    else
+	       sec_max_size = dataset_size[i];
 	}
-          //printf("offset[0] : %llu \n", offset[0]);
-          status = H5Sselect_hyperslab(filespace,H5S_SELECT_SET,offset,NULL,count,block);       
+	
+    }
+    
+    /* get local data size and allocate */
+    howmany =num_dataset_obj;
+    n0 = max_size+sec_max_size;  // here data_size[0] should be replaced by the maximum of datasize array elements
+    dataset_per_proc=(n0/mpi_size)+0.5;   
+    printf("dataset_per_proc : %d \n", (int) dataset_per_proc);
+    printf("howmany is : %d \n", (int) howmany);
 
+    /* Create memory space and allocate data buffer*/
+    tot_proc_datasets[0]= dataset_per_proc * howmany * 2; // number of columns, multiplied by two to hold complex
+    tot_proc_datasets[1]= 1; // number of rows 
+    memspace = H5Screate_simple(2,tot_proc_datasets,NULL);
+   // dset_data = (double complex *)malloc(tot_proc_datasets[0]*size);//always double type data
+
+    
+    printf("fftw n0 is %d \n",(int) n0);
+    alloc_local = fftw_mpi_local_size_many_1d(n0, howmany, MPI_COMM_WORLD, FFTW_FORWARD, FFTW_ESTIMATE, &local_ni, &local_i_start, &local_n0, &local_0_start);
+
+    printf("local_ni : %d \n",(int) local_ni);
+    printf("local_i_start : %d \n",(int) local_i_start);
+    printf("local_n0 : %d \n",(int) local_n0);
+    printf("local_0_start : %d \n",(int) local_0_start);
+
+    printf("fftw allocated size is %d \n",(int) alloc_local);
+
+    data = fftw_alloc_complex(alloc_local);
+
+    //printf("fftw allocated size is %d \n",(int) alloc_local);
+    
+    /* create plan for in-place forward DFT */
+    plan = fftw_mpi_plan_many_dft(1, &n0, howmany,FFTW_MPI_DEFAULT_BLOCK,FFTW_MPI_DEFAULT_BLOCK, data, data, MPI_COMM_WORLD,FFTW_FORWARD, FFTW_ESTIMATE); 
+   if(plan == NULL) printf("plan is null \n");
+    
+    if(iscomplex == true) set = 2;
+
+    for(i=0;i<num_dataset_obj; i++)
+    { 
+	   //if(i=0) data = fftw_alloc_complex(alloc_local);
+ 
+          /*Create the filespace/dataset hyperslab*/
+          printf("dataset[%d] size is %d \n",i,(int) dataset_size[i]);
+          if(local_i_start + local_ni < dataset_size[i])
+	  {                               
+         	offset[0]=mpi_rank*local_ni*set;// starting offset is multiplied by two because each complex number is two values
+          	offset[1]=0;
+          	count[0] = local_ni; // represents the number of complex numbers
+	  	count[1] = 1;
+		block[0]= set; //each block of 2 values represents one complex number
+                block[1] = 1;
+	        status = H5Sselect_hyperslab(filespace[i],H5S_SELECT_SET,offset,NULL,count,block);	
+	  }
+	  
+	  if(local_i_start < dataset_size[i] && local_i_start + local_ni > dataset_size[i])
+	  {                               
+         	offset[0]=mpi_rank*local_ni*set;// starting offset is multiplied by two because each complex number is two values
+          	offset[1]=0;
+          	count[0] = dataset_size[i]-local_i_start ; // represents the number of complex numbers
+	  	count[1] = 1;
+		block[0]= set; //each block of 2 values represents one complex number
+                block[1] = 1;
+                status = H5Sselect_hyperslab(filespace[i],H5S_SELECT_SET,offset,NULL,count,block);	
+	  }
+		
+	  if(local_i_start > dataset_size[i]) //all zeros
+	  {
+	       for(j=0;j<local_ni; j++)
+    	       {
+		  dset_data[i]= 0.0;
+	       }
+
+	  }
           
-          /* Create memory space and allocate data buffer*/
-	  tot_proc_datasets[0]= dataset_per_proc * howmany * 2; // number of columns, multiplied by two to hold complex
-          tot_proc_datasets[1]= 1; // number of rows 
-          if(obj_idx == 0){  // create memory space and allocate buffer
-               memspace = H5Screate_simple(2,tot_proc_datasets,NULL); //could this go outside loop?
-               dset_data = (double complex *)malloc(tot_proc_datasets[0]*size);//always double type data
-          }
-
+          printf("count[0] : %llu \n", count[0]);
+          
           /*Create the memory hyperslab to transfer data from the currently opened dataset*/   
 
-          //count[0]= dataset_per_proc; 
-          //count[1] = 1;
-          offset[0]= (num_dataset_obj-1)*2; //num_dataset_obj-1 is the index of the dataset object if a list containing only dataset objects existed 
+          offset[0]= i*2; //i is the index of the dataset object if a list containing only dataset objects existed 
           offset[1] = 0;
-          stride[0]= num_obj*2;
+          stride[0]= num_dataset_obj*2;
           stride[1] = 1;
+          block[0]= 1; //each block of 2 values represents one complex number
+          block[1] = 1;
           
-          //block[0]= 2; //each block of 2 values represents one complex number
-          //block[1] = 1;
           status = H5Sselect_hyperslab(memspace,H5S_SELECT_SET,offset,stride,count,block);
 
 
           plist_id = H5Pcreate(H5P_DATASET_XFER);
           H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
       
-         // if(obj_idx == 0){
-           //  dset_data = (double complex *)malloc(num_obj*dataset_per_proc*size);//always double type data
-             //dset_data = (datatype *)malloc(num_obj*size_per_proc*size); 
-         // }
       
           /* Read the dataset. */
-          status = H5Dread(dataset_id, H5T_IEEE_F64LE, memspace, filespace, H5P_DEFAULT, dset_data);
+          status = H5Dread(dataset_id[i], H5T_IEEE_F64LE, memspace, filespace[i], H5P_DEFAULT, data);
 
-          if(mpi_rank == 0 && obj_idx == 1){
-    	    for (i=0; i<20;i++){
-        	    printf("%f",creal(dset_data[1999-i]));
+          if(mpi_rank == 0 && i == 1){
+    	    for (j=0; j<10;j++){
+        	    printf("%f",creal(data[j]));
 		    printf("\n ");
     	    }
           }
-
-          ++obj_idx;
      
           /* Close/release resources HDF dataset related resources*/
-          H5Dclose(dataset_id);
-          H5Sclose(filespace);
-        }
+          H5Dclose(dataset_id[i]);
+          H5Sclose(filespace[i]);
       
       
     }
 
     /*HDF5 file read end*/
-    
-   
-
-   
-    /* get local data size and allocate */
-    howmany = 2;
-    n0 = 2*dataset_size[0];  // here data_size[0] should be replaced by the maximum of datasize array elements
-    //tot_buffer_size = howmany*n0;
-      //local_ni = proc_size;
-    //local_i_start = offset[0];
-
-    printf("fftw n0 is %d \n",(int) n0);
-    alloc_local = fftw_mpi_local_size_many_1d(n0, howmany, MPI_COMM_WORLD, FFTW_FORWARD, FFTW_ESTIMATE, &local_ni, &local_i_start, &local_n0, &local_0_start);
-    printf("local_ni : %d \n",(int) local_ni);
-    printf("local_i_start : %d \n",(int) local_i_start);
-    printf("local_n0 : %d \n",(int) local_n0);
-    printf("local_0_start : %d \n",(int) local_0_start);
-
-    data = fftw_alloc_complex(alloc_local);
-
-    printf("fftw allocated size is %d \n",(int) alloc_local);
-
-    
-
-   
-    //for one-dimensional transforms, only composite (non-prime) n0 are currently supported (unlike the serial FFTW). Requesting an unsupported transform size will yield a NULL plan. (As in the serial interface, highly composite sizes generally yield the best performance.)  
-    /* create plan for in-place forward DFT */
-    plan = fftw_mpi_plan_many_dft(1, &n0, howmany,FFTW_MPI_DEFAULT_BLOCK,FFTW_MPI_DEFAULT_BLOCK, data, data, MPI_COMM_WORLD,FFTW_FORWARD, FFTW_ESTIMATE); 
-   if(plan == NULL)
-      printf("plan is null \n");
-
-    /* initialize data to some function my_function(x,y) */
     printf("Input  data: \n");
-    //data = (fftw_complex *) dset_data;
-  
-    if(local_i_start + local_ni-1 < dataset_size[0]) //not yet reached 0 appendinging region
-    {
-    	for (i = 0; i < howmany*local_ni; i++) {
-            	data[i] = dset_data[i]; 
-            	if(i<10) printf("  %f + i%f \n", creal(data[i]), cimag(data[i]));
-     	}
-    }
-
-    if(local_i_start < dataset_size[0] && local_i_start + local_ni-1 > dataset_size[0] )  //dataset size 0 used but normally should be the maximum value in the datasert size array
-    {
-        printf("  Inside second if part \n");
-    	for (i = 0; i < howmany * dataset_size[0]; i++) {
-            	data[i] = dset_data[i]; 
-            	//if(i<10) printf("  %f + i%f \n", creal(data[i]), cimag(data[i]));
-     	}
-	
-	for (i = howmany*dataset_size[0]; i < howmany*local_ni; i++) { //append zeros for the final data values
-            	data[i] =  0; 
-            	//if(i<10) printf("  %f + i%f \n", creal(data[i]), cimag(data[i]));
-     	}
-    }
-
-    if(local_i_start > dataset_size[0]) //append zeros for the final data values
-    {
-    	for (i = 0; i < howmany*local_ni; i++) {
-            	data[i] = 0; 
-            	if(i<10) printf("  %f + i%f \n", creal(data[i]), cimag(data[i]));
-     	}
-    }
-
-  /* if(i = 0; i < howmany*local_ni; i++) //not yet reached 0 appendinging region
-    {
-    	for (i = 0; i < howmany*local_ni; i++) {
-            	data[i] = dset_data[i]; 
-            	if(i<10) printf("  %f + i%f \n", creal(data[i]), cimag(data[i]));
-     	}
-    }*/
+    //data =   dset_data;
+    for (i = 0; i < 10; i++) {
+         if(i<10) printf("  %f + i%f \n", creal(data[i]), cimag(data[i]));
+     }
 
    /* compute transforms, in-place, as many times as desired */
    fftw_execute(plan);
@@ -321,7 +298,6 @@ main (int argc, char **argv)
    /* create plan for in-place forward DFT */
     plan2 = fftw_mpi_plan_many_dft(1, &n0, howmany,FFTW_MPI_DEFAULT_BLOCK,FFTW_MPI_DEFAULT_BLOCK, multiplied_fft, multiplied_fft, MPI_COMM_WORLD,FFTW_BACKWARD, FFTW_ESTIMATE); 
 
-   int j;
    int set_base_idx = 0;
    int index = 0;
    while(set_base_idx < signal_num*length_fft) //set index - begining of a signal fft group with the same index before multiplication
